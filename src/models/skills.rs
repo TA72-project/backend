cb0c9c53f233acd60293482c37b9f004ba634ac1
Valentin271 <valentin.d77@free.fr>
2023-10-15 20:02:24 +0200

    feat: some skills routes

--- src/models/skills.rs
+++ src/models/skills.rs
@@ -1,4 +1,23 @@
+use diesel::{AsChangeset, Insertable, Queryable};
+use serde::{Deserialize, Serialize};
+
+use crate::schema::skills;
+
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = skills)]
 pub struct Skill {
-    id: u64,
+    id: i64,
+    name: String,
+}
+
+#[derive(Debug, Clone, Deserialize, AsChangeset)]
+#[diesel(table_name = skills)]
+pub struct UpdateSkill {
+    name: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Insertable)]
+#[diesel(table_name = skills)]
+pub struct NewSkill {
     name: String,
 }
--- src/routes/skills.rs
+++ src/routes/skills.rs
@@ -1,10 +1,45 @@
-use actix_web::{get, web, Responder, Scope};
+use actix_web::{
+    get, post,
+    web::{self, Json},
+    Responder, Scope,
+};
+use diesel::{insert_into, QueryDsl, RunQueryDsl};
+
+use crate::{
+    database::DbPool,
+    error::Result,
+    models::{NewSkill, Skill},
+    schema::skills,
+};
 
 pub fn routes() -> Scope {
-    web::scope("/skills").service(get)
+    web::scope("/skills")
+        .service(all)
+        .service(get)
+        .service(post)
+}
+
+#[get("")]
+async fn all() -> impl Responder {
+    "Skills list".to_owned()
 }
 
 #[get("/{id}")]
-async fn get(id: web::Path<u64>) -> impl Responder {
-    format!("{}", id)
+async fn get(id: web::Path<i64>, pool: web::Data<DbPool>) -> Result<impl Responder> {
+    let res: Skill =
+        web::block(move || skills::table.find(*id).get_result(&mut pool.get().unwrap())).await??;
+
+    Ok(Json(res))
+}
+
+#[post("")]
+async fn post(new_skill: web::Json<NewSkill>, pool: web::Data<DbPool>) -> Result<impl Responder> {
+    let skill: Skill = web::block(move || {
+        insert_into(skills::table)
+            .values(&new_skill.0)
+            .get_result(&mut pool.get().unwrap())
+    })
+    .await??;
+
+    Ok(Json(skill))
 }

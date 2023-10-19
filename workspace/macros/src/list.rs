42856d85a38a9ccfed480b2e0f8a8ff68dd65fee
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2023-10-19 08:06:53 +0200

    feat: macros utils (#4)
    I added a new crate to define macros.
    
    I defined macros to help reduce boilerplate code for some redundant
    queries.

---
+++ workspace/macros/src/list.rs
@@ -0,0 +1,28 @@
+/// A highly specialized macro that queries a table for a list of entries.
+///
+/// # Parameters
+///
+/// - The schema to execute the query against
+/// - The database connections pool
+/// - The query parameters (used for paging)
+///
+/// # Example
+///
+/// ```ignore
+/// use macros::list;
+///
+/// let skills: Vec<Skill> = list!(schema::skills, pool, query);
+/// ```
+#[macro_export]
+macro_rules! list {
+    ($schema:ident, $pool:expr, $query:expr) => {
+        actix_web::web::block(move || {
+            $schema::table
+                .select($schema::all_columns)
+                .offset($query.offset().into())
+                .limit($query.limit().into())
+                .get_results(&mut $pool.get().unwrap())
+        })
+        .await??
+    };
+}

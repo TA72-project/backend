2c3a5e063b1dc9401dacda458681a03f7f5a8f48
Valentin271 <valentin.d77@free.fr>
2023-10-22 21:46:16 +0200

    feat: `visits` & `l_visits_nurses` migrations

---
+++ migrations/2023-10-22-071013_create_visits/up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "visits" (
+  "id" bigserial PRIMARY KEY,
+  "start" timestamp NOT NULL,
+  "end" timestamp NOT NULL,
+  "id_mission" bigint NOT NULL REFERENCES "missions" ("id")
+);
+
---
+++ migrations/2023-10-22-072021_create_l_visits_nurses/down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS "l_visits_nurses";

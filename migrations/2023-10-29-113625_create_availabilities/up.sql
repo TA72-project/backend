e36e00e8080254bdb6521519cc32cfc22274f086
Valentin271 <valentin.d77@free.fr>
2023-10-29 12:40:46 +0100

    feat: availabilities migration

---
+++ migrations/2023-10-29-113625_create_availabilities/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "availabilities" (
+  "id" bigserial PRIMARY KEY,
+  "start" timestamp NOT NULL,
+  "end" timestamp NOT NULL,
+  "recurrent" bool NOT NULL,
+  "id_nurse" bigint NOT NULL REFERENCES "nurses" ("id")
+);
+

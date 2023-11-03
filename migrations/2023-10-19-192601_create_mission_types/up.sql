cb9d7ae70628a408f27deeb14289f80e4e0def24
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2023-11-03 12:26:35 +0100

    feat: added mission duration fields (#22)
    I added fields to `missions` and `mission_types` to represent the
    duration of a mission. It is usually different from `start` and `end` as
    these represent the time window allowed for the mission.

--- migrations/2023-10-19-192601_create_mission_types/up.sql
+++ migrations/2023-10-19-192601_create_mission_types/up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "mission_types" (
      "id" bigserial PRIMARY KEY,
      "name" text UNIQUE NOT NULL,
-     "people_required" smallint NOT NULL DEFAULT 1
+     "people_required" smallint NOT NULL DEFAULT 1,
+     "minutes_duration" int NOT NULL
 );
